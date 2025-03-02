33f6c69866468b0ebea77373c9666e3fb78ef7b2
Björn Förschner <bjoern@foerschner.dev>
2025-03-02 18:17:28 +0100

    fix(rust): fix broken cargo installs by sourcing cargo/env

--- init_scripts/rustAndCargoTools.sh
+++ init_scripts/rustAndCargoTools.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
 
+source $HOME/.cargo/env
 cargo install eza
 cargo install du-dust

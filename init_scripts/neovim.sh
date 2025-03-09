5c49153be24606f1483df3def4e25ba3a1d96328
Björn Förschner <1569796+BFoerschner@users.noreply.github.com>
2025-03-09 11:11:23 +0100

    ref(nvim): rename venvs/ dir to .venvs/

--- init_scripts/neovim.sh
+++ init_scripts/neovim.sh
@@ -4,8 +4,8 @@ SLEEP_SECONDS=60
 
 nvim --headless '+Lazy install' +q
 npm install -g neovim
-mkdir "$HOME/venvs" && python3 -m venv "$HOME/venvs/.nvim-venv"
-source "$HOME/venvs/.nvim-venv/bin/activate" && python3 -m pip install pynvim neovim
+mkdir "$HOME/.venvs" && python3 -m venv "$HOME/.venvs/.nvim-venv"
+source "$HOME/.venvs/.nvim-venv/bin/activate" && python3 -m pip install pynvim neovim
 
 LSP_CFG=(
   "docker-compose-language-service"

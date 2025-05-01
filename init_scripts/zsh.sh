1da6e0622ae37f0476dc07f838def36e74c61885
Björn Förschner <1569796+BFoerschner@users.noreply.github.com>
2025-05-01 12:33:07 +0000

    feat(zsh): Use actual .zshrc for plugin installation and generate completion-cache

--- init_scripts/zsh.sh
+++ init_scripts/zsh.sh
@@ -1,44 +1,18 @@
-#!/bin/zsh
-export TERM=xterm-256color
+#!/usr/bin/env zsh
+# Fail fast
+set -euo pipefail
 
-# Set the directory we want to store zinit and plugins
-ZINIT_HOME="${HOME}/.local/share/zinit/zinit.git"
+export ZDOTDIR="${ZDOTDIR:-$HOME}"
+export ZSHRC_FORCE_LOAD=1 # Override non-interactive early return
 
-# Download Zinit, if it's not there yet
-if [ ! -d "$ZINIT_HOME" ]; then
-  mkdir -p "$(dirname "$ZINIT_HOME")"
-  git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
-fi
+# Source your actual zshrc
+source "$ZDOTDIR/.zshrc"
 
-# Source/Load zinit
-source "${ZINIT_HOME}/zinit.zsh"
+# Force compinit and dump file generation
+autoload -Uz compinit
+compinit -i
 
-zinit light zsh-users/zsh-completions
-zinit light zsh-users/zsh-syntax-highlighting
-zinit light zsh-users/zsh-autosuggestions
-zinit light Aloxaf/fzf-tab
-zinit light birdhackor/zsh-eza-ls-plugin
-autoload -Uz compinit && compinit
+[[ -f "${ZDOTDIR}/.zcompdump" ]] && zcompile "${ZDOTDIR}/.zcompdump"
 
-# Terraform autocomplete
-autoload -U +X bashcompinit && bashcompinit
-complete -o nospace -C "$(which terraform)" terraform
-
-zinit snippet OMZL::git.zsh
-zinit snippet OMZP::git
-zinit snippet OMZP::sudo
-zinit snippet OMZP::archlinux
-zinit snippet OMZP::aws
-zinit snippet OMZP::kubectl
-zinit snippet OMZP::kubectx
-zinit snippet OMZP::command-not-found
-zinit snippet OMZP::ansible
-zinit snippet OMZP::brew
-zinit snippet OMZP::docker/completions/_docker
-zinit snippet OMZP::docker-compose
-zinit snippet OMZP::extract
-zinit snippet OMZP::gitignore
-zinit snippet OMZP::git-commit
-
-# reload snippets every new shell
-zinit cdreplay -q
+#!/usr/bin/env zsh
+echo "ZSH completion-cache successfully generated"

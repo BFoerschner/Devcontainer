3994b33d08eaefed26649f2457dc552a5903d4a8
Björn Förschner <1569796+BFoerschner@users.noreply.github.com>
2025-07-15 20:51:17 +0000

    fix(zsh-init): fixed incorrect zsh initialization script

--- init_scripts/zsh_init.sh
+++ init_scripts/zsh_init.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env zsh
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/logging.sh
 
-curl -sS https://starship.rs/install.sh | sh
+curl -sS https://starship.rs/install.sh | zsh -s -- -y
 export ZSHRC_FORCE_LOAD=1
 
 log "Initializing Zsh environment"
+
 source ~/.zshrc # sourcing the file should do the trick
+
+zinit update

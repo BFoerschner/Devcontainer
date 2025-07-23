9dee915a39b8cc06a3c31aff0152c73cdefc4572
Björn Förschner <1569796+BFoerschner@users.noreply.github.com>
2025-07-23 13:47:36 +0000

    refactor(cleanup): cleans up temp and cache files+dirs. Also checks now if os is ubuntu before running apt-get clean etc.

--- build-scripts/cleanup_caches.sh
+++ build-scripts/cleanup_caches.sh
@@ -5,7 +5,17 @@ cleanup_caches() {
   go clean -modcache
   uv cache clean
   [ -d "$HOME"/.cargo/registry ] && rm -rf "$HOME"/.cargo/registry
-  apt-get autoremove -y &&
-    apt-get clean &&
-    apt-get autoclean
+
+  # if apt-get exists, then clean cache
+  if command -v apt-get &>/dev/null; then
+    apt-get clean -y
+    apt-get autoclean -y
+    apt-get autoremove -y
+  fi
+
+  # clean logs, temp files and caches
+  find /var/log -type f -delete || true
+  find /var/log -type d -empty -delete || true
+  find /root/.cache -type f -delete || true
+  find /root/.cache -type d -empty -delete || true
 }

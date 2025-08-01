779a4d22b423d6087f5b796dbd64481a8bf06786
Björn Förschner <1569796+BFoerschner@users.noreply.github.com>
2025-08-02 01:59:12 +0200

    fix(config): remove default bashrc if it's there

--- build-scripts/dotfiles.sh
+++ build-scripts/dotfiles.sh
@@ -4,6 +4,7 @@
 install_dotfiles() {
   [ -d "$HOME"/.dotfiles ] && rm -rf "$HOME"/.dotfiles
   [ -f "$HOME"/.zshrc ] && rm "$HOME"/.zshrc
+  [ -f "$HOME"/.bashrc ] && rm "$HOME"/.bashrc
   git clone https://github.com/BFoerschner/dotfiles "$HOME"/.dotfiles
   cd "$HOME"/.dotfiles && stow .
 }

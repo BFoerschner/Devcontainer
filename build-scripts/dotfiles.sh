ed966313c67b894ee87477fc8714cbb79db7406d
Björn Förschner <1569796+BFoerschner@users.noreply.github.com>
2025-08-17 10:39:33 +0000

    refactor(build): simplify chezmoi installation

--- build-scripts/dotfiles.sh
+++ build-scripts/dotfiles.sh
@@ -1,19 +1,5 @@
-#!/usr/bin/env ${1|bash,sh|}
-
-install_chezmoi() {
-  log "Installing chezmoi"
-  TMP_DIR=$(mktemp -d)
-  cd "$TMP_DIR" || exit
-  git clone https://github.com/twpayne/chezmoi.git
-  cd chezmoi || exit
-  latest_tag=$(git tag --sort=-version:refname | head -1)
-  git checkout "$latest_tag"
-  make install-from-git-working-copy
-  rm -rf "$TMP_DIR"
-}
+#!/usr/bin/env bash
 
 install_dotfiles() {
-  install_chezmoi
-
-  cd "$HOME" && chezmoi init --apply BFoerschner
+  sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME"/.local/bin init --apply "BFoerschner"
 }
